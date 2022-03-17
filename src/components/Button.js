bd5d201c0c84c78e142c20ff8c845ce2ae0ee151
Thomas Pisfil <thomaspisfil@TP-MacBook.local>
2022-03-17 16:25:31 -0400

    Added button functionality and styling

--- src/components/Button.js
+++ src/components/Button.js
@@ -12,7 +12,7 @@ const Button = ({children, type, onClick, buttonStyle, buttonSize}) => {
     
     return (
         <Link to='/sign-up' className='btn-mobile'>
-            <button>
+            <button className={`btn ${checkButtonStyle} ${checkButtonSize}`} onClick={onClick} type={type}>
                 {children}
             </button>
         </Link>
