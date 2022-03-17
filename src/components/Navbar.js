89d1ff9b9ba37e0a292c9d571466769ef5237de4
tpisfil <tpisfil13@gmail.com>
2022-03-17 17:09:04 -0400

    Working on Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -24,7 +24,6 @@ const Navbar = () => {
             setButton(true);
         }
     }; //determines whether the button will be shown or not 
-    //testing again
 
     window.addEventListener('resize', showButton);
     //if the window gets resized to smaller than 960 it will set button to false 
