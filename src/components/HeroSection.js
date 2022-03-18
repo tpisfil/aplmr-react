513172a3b272f5d0f99c9b9241a882bfea70aff2
tpisfil <tpisfil13@gmail.com>
2022-03-18 15:28:40 -0400

    Fixed Routing issue

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import '../App.css';
 import Button from './Button';
-import './Main.css';
+import './HeroSection.css';
 
-const Main = () => {
+const HeroSection = () => {
   return (
     <div className='hero-container'>
         <video src="/videos/video-2.mp4" autoPlay loop muted />
@@ -11,9 +11,10 @@ const Main = () => {
         <p>What are you waiting for?</p>
         <div className="hero-btns">
             <Button className="btns" buttonStyle='btn--outline' buttonSize="btn--large">GET STARTED</Button>
+            <Button className="btns" buttonStyle='btn--primary' buttonSize="btn--large">WATCH TRAILER <i className='far fa-play-circle'/></Button>
         </div>
     </div>
   )
 }
 
-export default Main
+export default HeroSection
