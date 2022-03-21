9939a2f387f636c75ad570ed62aac9f7fafb6669
tpisfil <tpisfil13@gmail.com>
2022-03-21 17:52:17 -0400

    Working on Cards...

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -6,13 +6,20 @@ function Cards() {
   return (
     <div className='cards'>
         <h1>Check out these EPIC destinations</h1>
+        
         <div className="cards__container">
             <div className="cards__wrapper">
                 <ul className="cards__items">
                     <CardItem src="images/img-9.jpg" text="Explore the hidden waterfall deep inside the Amazon jungle" label="Adventure" path='/services'/>
+                    <CardItem src="images/img-2.jpg" text="Travel through the isalnds of Bali in a private cruise" label="Luxury" path='/services'/>
+                </ul>                
+                <ul className="cards__items">
+                    <CardItem src="images/img-9.jpg" text="Explore the hidden waterfall deep inside the Amazon jungle" label="Adventure" path='/services'/>
+                    <CardItem src="images/img-2.jpg" text="Travel through the isalnds of Bali in a private cruise" label="Luxury" path='/services'/>
+                    <CardItem src="images/img-9.jpg" text="Explore the hidden waterfall deep inside the Amazon jungle" label="Adventure" path='/services'/>
                 </ul>
             </div>
-        </div>
+        </div>        
     </div>
   )
 }
